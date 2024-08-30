b555ccb199cbc46461f806491c16e0011bfd47a1
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-08-30 00:39:10 -0700

    testing nwb file, and experimenting with yaml format

---
+++ nwb_linkml/tests/test_io/test_io_nwb.py
@@ -0,0 +1,17 @@
+"""
+Placeholder test module to test reading from pynwb-generated NWB file
+"""
+
+
+def test_read_from_nwbfile(nwb_file):
+    """
+    Read data from a pynwb HDF5 NWB file
+    """
+    pass
+
+
+def test_read_from_yaml(nwb_file):
+    """
+    Read data from a yaml-fied NWB file
+    """
+    pass
